2607238503a2b1c9a8534e993b111b31b0789c7f
Boris Manojlovic <boris@steki.net>
2018-03-01 20:10:26 +0100

    add golang version, moved c version into c-src

---
+++ cmd/root.go
@@ -0,0 +1,120 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var serverName, messageText, receiverNumber, port, sendsmsURL string
+var verbose, checkSentStatus bool
+
+func init() {
+	//	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVarP(&serverName, "servername", "s", "", "Server hosting smstools")
+	rootCmd.PersistentFlags().StringVarP(&messageText, "messagetext", "t", "", "")
+	rootCmd.PersistentFlags().StringVarP(&port, "port", "p", "80", "Port Number")
+	rootCmd.PersistentFlags().StringVarP(&receiverNumber, "receivernumber", "b", "", "Receiver number")
+	// rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	rootCmd.PersistentFlags().BoolVarP(&checkSentStatus, "checksentstatus", "c", false, "verbose output")
+}
+
+// func initConfig() {
+// 	//
+// }
+
+var rootCmd = &cobra.Command{
+	Use:   "sendsms",
+	Short: "sending sms from command line trough smstools3 server implementation",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("requires at least one arg")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(messageText) == 0 {
+			log.Fatal("Text to send is mandatory")
+		} else if len(receiverNumber) == 0 {
+			log.Fatal("receivernumber is mandatory")
+		} else if len(serverName) == 0 {
+			log.Fatal("Servername is mandatory")
+		}
+		// }
+		var netClient = &http.Client{
+			Timeout: time.Second * 10,
+		}
+
+		sendsmsURL := "http://" + serverName + ":" + port + "/sendsms.php"
+		dataPostMain := url.Values{}
+		dataPostMain.Set("sent", "cli")
+		dataPostMain.Add("fromform", "true")
+		dataPostMain.Add("to", receiverNumber)
+		dataPostMain.Add("text", messageText)
+		if checkSentStatus == true {
+			dataPostMain.Add("confirmation", "on")
+		}
+		r, _ := http.NewRequest("POST", sendsmsURL, strings.NewReader(dataPostMain.Encode()))
+		r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		resp, err := netClient.Do(r)
+		if err != nil {
+			log.Error("Server responded with error", err)
+		}
+		defer resp.Body.Close()
+		mainBody, _ := ioutil.ReadAll(resp.Body)
+		mainText := string(mainBody)
+		if checkSentStatus == true {
+			for i := 0; i < 60; i++ {
+				dataPostCheck := url.Values{}
+				dataPostCheck.Add("sent", "cli")
+				dataPostCheck.Add("confirmation", "on")
+				dataPostCheck.Add("msgid", mainText[strings.Index(mainText, "#")+1:])
+				r1, _ := http.NewRequest("POST", sendsmsURL, strings.NewReader(dataPostCheck.Encode()))
+				r1.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+				resp1, err := netClient.Do(r1)
+				if err != nil {
+					log.Error("Server responded with error", err)
+					defer resp1.Body.Close()
+				}
+				checkBody, _ := ioutil.ReadAll(resp1.Body)
+				checkText := string(checkBody)
+				if strings.Contains(checkText, "FAILED") == true {
+					log.Fatal("Sending failed: ", checkText)
+				} else if strings.Contains(checkText, "SENT") == true {
+					log.Info("Message Sent: ", checkText)
+					break
+				}
+				time.Sleep(time.Second)
+			}
+		} else {
+			log.Info("Message Sent: ", mainText)
+		}
+		// if verbose == true {
+		// 	log.WithFields(log.Fields{
+		// 		"serverName":      serverName,
+		// 		"messageText":     messageText,
+		// 		"receiverNumber":  receiverNumber,
+		// 		"verbose":         verbose,
+		// 		"checkSentStatus": checkSentStatus,
+		// 		"sendsmsURL":      sendsmsURL,
+		// 	}).Warn("Configured command flags")
+		// }
+
+	},
+}
+
+// Execute function of command line argument parsing
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/bmanojlovic/sendsms/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
